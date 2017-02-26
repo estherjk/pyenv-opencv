216fdb24d879383466745b7688c0221a839d085c
Esther Jun Kim <esther.jun.kim@gmail.com>
2017-02-25 18:05:11 -0800

    Update text in Exception

--- detection.py
+++ detection.py
@@ -5,7 +5,7 @@ import sys
 # Update path to the Haar cascades file if necessary, e.g. if OpenCV version is different
 faceCascadeFile = '/usr/local/Cellar/opencv/2.4.13.2/share/OpenCV/haarcascades/haarcascade_frontalface_default.xml'
 faceCascade = cv2.CascadeClassifier(faceCascadeFile)
-if faceCascade.empty(): raise Exception('cascade is empty. Double-check the path.')
+if faceCascade.empty(): raise Exception('faceCascade is empty. Double-check the path.')
 
 cap = cv2.VideoCapture(0)
 cap.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, 640)
