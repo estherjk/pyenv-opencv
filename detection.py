40ee0df5e2659ff546cc1115c31c1a7ed6ac0859
Esther Jun Kim <hello@esthermakes.tech>
2019-08-09 19:08:42 -0700

    Update project for Python 3 & OpenCV 4
    The README also points to the Wiki, which contains the updated setup instructions.

--- detection.py
+++ detection.py
@@ -3,16 +3,17 @@ import cv2
 import sys
 
 # Update path to the Haar cascades file if necessary, e.g. if OpenCV version is different
-faceCascadeFile = '/usr/local/Cellar/opencv/2.4.13.2/share/OpenCV/haarcascades/haarcascade_frontalface_default.xml'
+faceCascadeFile = '/usr/local/opt/opencv/share/opencv4/haarcascades/haarcascade_frontalface_default.xml'
 faceCascade = cv2.CascadeClassifier(faceCascadeFile)
 if faceCascade.empty(): raise Exception('faceCascade is empty. Double-check the path.')
 
 cap = cv2.VideoCapture(0)
-cap.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, 640)
-cap.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 480)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
 while(True):
     ret, frame = cap.read()
+
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = faceCascade.detectMultiScale(gray)
 
